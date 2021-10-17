0ee7ff2058fc2aa64f37ee5bdbb126224a6a478a
Matthew Bowman <mbowman27x@gmail.com>
2021-10-17 00:10:16 -0400

    new Commit Saturday

--- projectzero/src/main/scala/example/RunPokedex.scala
+++ projectzero/src/main/scala/example/RunPokedex.scala
@@ -10,6 +10,8 @@ import org.mongodb.scala.model.Aggregates._
 import org.mongodb.scala.model.Updates._
 import example.Helpers._
 
+import scala.collection.mutable.SortedSet
+
 object RunPokedex {
     def main(args:Array[String]){
         println("Starting MongoDB - Scala")
@@ -17,7 +19,6 @@ object RunPokedex {
 
         while(true){
             selection
-            println("Current ID value is: " + id)
         }    
     }
 
@@ -44,25 +45,28 @@ object RunPokedex {
                                "Speed" -> Speed )
         )
 
-        document.foreach(println)
-
         val observable: Observable[Completed] = collection.insertOne(document)
-        println("Creating Pokemon: " + pokemonName)
         collection.insertOne(document).results()
+        println("Creating Pokemon: " + pokemonName)
+        collection.find(equal("name.english", pokemonName)).printResults()
     }
 
     def selection{
         println("Select an option")
-        var setOne: Set[String] = Set("1: Query the Pokedex Database", "2: Create a Pokemon", "3: Import Your Own Pokemon! (Read JSON file and Upload to Database)")
-
+        var setOne: SortedSet[String] = SortedSet("1: Query the Pokedex Database", "2: Create a Pokemon", "3: Update an existing Pokemon", "4: Delete a Pokemon", "5: Import a Pokemon") 
+        
         for(x <- setOne){
             println(x)
         }
 
         var num = 0
+        var num2 = 0
+        var name = ""
+        var newValue = 0
+        var newValue2 = ""
         var x = 0
         var y = 0
-        var name = ""
+        
         num = scala.io.StdIn.readInt
 
         num match{
@@ -89,7 +93,7 @@ object RunPokedex {
                                             collection.find(equal("id", z)).printResults()
                                         }
                             case 5 => println("Enter a name (first letter capitalized)")
-                                        name = scala.io.StdIn.readLine()
+                                        name = scala.io.StdIn.readLine
                                         println("Here is the Pokemon you were looking for")
                                         collection.find(equal("name.english", name)).printResults()
                             case _ => println("Enter a valid number, returning to Query Selection...")
@@ -99,19 +103,115 @@ object RunPokedex {
             case 2 => println("You are in the Pokemon Creation Interface")
                         RunPokedex.id += 1
                         println("Type in a name for your Pokemon")
-                        pokemonName = scala.io.StdIn.readLine()
+                        pokemonName = scala.io.StdIn.readLine
                         println("Enter a number for HP: ")
-                        HP = scala.io.StdIn.readInt()
+                        HP = scala.io.StdIn.readInt
                         println("Enter a number for Attack Stat: ")
-                        Attack = scala.io.StdIn.readInt()
+                        Attack = scala.io.StdIn.readInt
                         println("Enter a number for Defense Stat: ")
-                        Defense = scala.io.StdIn.readInt()
+                        Defense = scala.io.StdIn.readInt
                         println("Enter a number for Speed Stat: ")
-                        Speed = scala.io.StdIn.readInt()    
+                        Speed = scala.io.StdIn.readInt
 
-                        createDocObserve        
+                        createDocObserve      
 
-            case 3 => println("Enter a path for the JSON file you would like to UPLOAD")
+            case 3 => println("Select Pokemon by ID or Name")
+                        println("1: ID")
+                        println("2: Name")
+                        num = scala.io.StdIn.readInt
+
+                            num match{
+                                case 1 => println("Enter an ID")
+                                            num = scala.io.StdIn.readInt
+                                            collection.find(equal("id", num)).printResults()
+                                            println("Select a field to UPDATE")
+                                            println("1: id")
+                                            println("2: Name")
+                                            println("3: HP")
+                                            println("4: Attack")
+                                            println("5: Defense")
+                                            println("6: Speed")
+
+                                            num2 = scala.io.StdIn.readInt
+
+                                                num2 match{
+                                                    case 1 => println("Enter a value to replace the current value of id")
+                                                                newValue = scala.io.StdIn.readInt
+                                                                collection.updateOne(equal("id", num), set("id", newValue)).printResults()
+                                                                collection.find(equal("id", num)).printResults()
+                                                    case 2 => println("Enter a value to replace the current value of Name")
+                                                                newValue2 = scala.io.StdIn.readLine
+                                                                collection.updateOne(equal("id", num), set("name.english", newValue)).printResults()
+                                                                collection.find(equal("id", num)).printResults()
+                                                    case 3 => println("Enter a value to replace the current value of HP")
+                                                                newValue = scala.io.StdIn.readInt
+                                                                collection.updateOne(equal("id", num), set("base.HP", newValue)).printResults()
+                                                                collection.find(equal("id", num)).printResults()
+                                                    case 4 => println("Enter a value to replace the current value of Attack")
+                                                                newValue = scala.io.StdIn.readInt
+                                                                collection.updateOne(equal("id", num), set("base.Attack", newValue)).printResults()
+                                                                collection.find(equal("id", num)).printResults()
+                                                    case 5 => println("Enter a value to replace the current value of Defense")
+                                                                newValue = scala.io.StdIn.readInt
+                                                                collection.updateOne(equal("id", num), set("base.Defense", newValue)).printResults()
+                                                                collection.find(equal("id", num)).printResults()
+                                                    case 6 => println("Enter a value to replace the current value of Speed")
+                                                                newValue = scala.io.StdIn.readInt
+                                                                collection.updateOne(equal("id", num), set("base.Speed", newValue)).printResults()
+                                                                collection.find(equal("id", num)).printResults()
+                                                }
+
+                                case 2 => println("Enter a Name")
+                                            name = scala.io.StdIn.readLine
+                                            collection.find(equal("name.english", name)).printResults()
+                                            println("Select a field to UPDATE")
+                                            println("1: id")
+                                            println("2: Name")
+                                            println("3: HP")
+                                            println("4: Attack")
+                                            println("5: Defense")
+                                            println("6: Speed")
+
+                                            num2 = scala.io.StdIn.readInt
+
+                                                num2 match{
+                                                    case 1 => println("Enter a value to replace the current value of id")
+                                                                newValue = scala.io.StdIn.readInt
+                                                                collection.updateOne(equal("name.english", name), set("id", newValue)).printResults()
+                                                    case 2 => println("Enter a value to replace the current value of Name")
+                                                                newValue2 = scala.io.StdIn.readLine
+                                                                collection.updateOne(equal("name.english", name), set("name.english", newValue)).printResults()
+                                                    case 3 => println("Enter a value to replace the current value of HP")
+                                                                newValue = scala.io.StdIn.readInt
+                                                                collection.updateOne(equal("name.english", name), set("base.HP", newValue)).printResults()
+                                                    case 4 => println("Enter a value to replace the current value of Attack")
+                                                                newValue = scala.io.StdIn.readInt
+                                                                collection.updateOne(equal("name.english", name), set("base.Attack", newValue)).printResults()
+                                                    case 5 => println("Enter a value to replace the current value of Defense")
+                                                                newValue = scala.io.StdIn.readInt
+                                                                collection.updateOne(equal("name.english", name), set("base.Defense", newValue)).printResults()
+                                                    case 6 => println("Enter a value to replace the current value of Speed")
+                                                                newValue = scala.io.StdIn.readInt
+                                                                collection.updateOne(equal("name.english", name), set("base.Speed", newValue)).printResults()
+                                                }
+                            }
+            case 4 => println("Select Pokemon by ID or Name")
+                        println("1: ID")
+                        println("2: Name")
+                        num = scala.io.StdIn.readInt
+
+                        num match{
+                            case 1 => println("Enter an ID")
+                                            num = scala.io.StdIn.readInt
+                                            collection.find(equal("id", num)).printResults()
+                                            collection.deleteOne(equal("id", num)).printHeadResult(s"id $num was deleted")
+                            case 2 => println("Enter a Name")
+                                            name = scala.io.StdIn.readLine
+                                            collection.find(equal("name.english", name)).printResults()
+                                            collection.deleteOne(equal("name.english", name)).printHeadResult(s"$name was deleted")
+                        }
+
+            case 5 => println("Enter a path for the JSON file you would like to UPLOAD")
                         val fileName = scala.io.StdIn.readLine()
                         println("Reading JSON file...")
                         
