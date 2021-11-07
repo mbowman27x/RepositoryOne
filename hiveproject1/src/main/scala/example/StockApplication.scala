8052034e2901711448714f4d1feae520a39fb6b2
Matthew Bowman <mbowman27x@gmail.com>
2021-11-07 18:18:50 -0500

    11/7/2021 completed project1

--- hiveproject1/src/main/scala/example/StockApplication.scala
+++ hiveproject1/src/main/scala/example/StockApplication.scala
@@ -16,7 +16,7 @@ object StockApplication {
 
     def stocks(): Unit = {
 
-        var stockOptionSeq: Seq[String] = Seq("1: LOAD Stock Data from Alpha Vantage API to HDP", "2: LOAD API Data into Hive", "3: Query API Data", "4: Return to Main Screen")
+        var stockOptionSeq: Seq[String] = Seq("1: LOAD Stock Data from Alpha Vantage API to HDP", "2: LOAD/DROP API Data in Hive DataWarehouse", "3: Query API Data", "4: Return to Main Screen")
         println("Select an option")
 
         for(x <- stockOptionSeq){
@@ -82,7 +82,8 @@ object StockApplication {
         var hiveCommand = ""
         // statement.execute(hiveCommand)      <- this is for executing hive commands
         
-        println("Enter 1 to create Table")
+        println("Enter 1 to create new Table")
+        println("Enter 2 to drop a Table")
 
         val hiveSelection = scala.io.StdIn.readInt
 
@@ -97,6 +98,12 @@ object StockApplication {
                         hiveCommand = "LOAD DATA LOCAL INPATH '/tmp/dataFolder/stockData.csv' INTO TABLE " + newTable
                         statement.execute(hiveCommand)
                         println("Data loaded into Table successfully")
+
+            case 2 =>   println("Type in the name of the table you wish to drop")
+                        val dropName = scala.io.StdIn.readLine
+                        hiveCommand = "DROP TABLE " + dropName
+                        statement.execute(hiveCommand)
+                        println(dropName + " Table has been dropped from the database")
 
             case _ =>   println("Invalid: Returning to Selections")
         }
@@ -136,28 +143,112 @@ object StockApplication {
         var x = ""
         // x variable for printing queries in scala
 
+        println("Current Tables in the Database: ")
+            hiveCommand = "SHOW TABLES"
+            var z = statement.executeQuery(hiveCommand)
+            while (z.next()){
+                println(z.getString(1))
+            }
+
         println("Enter the ticker symbol of an existing Table")
         var tickerQuery = scala.io.StdIn.readLine
         println("Select a Query")
-        println("1: Look at most recent performance of " + tickerQuery)
-        println("2: ")
-        println("3: ")
-        println("4: ")
-        println("5: ")
-        println("6: ")
+        println("1: Show last 10 Trading Days of " + tickerQuery)
+        println("2: Show Max Price of " + tickerQuery + " in last 6 months")
+        println("3: Show Minimum Price of " + tickerQuery + " in last 6 months")
+        println("4: Show Average Daily High of " + tickerQuery + " in last 6 months")
+        println("5: Show Max Daily Volume Traded of " + tickerQuery + " in last 6 months")
+        println("6: Show Minimum Daily Volume Traded of " + tickerQuery + " in last 6 months")
+        println("7: Enter a custom Query")
 
         val querySelection = scala.io.StdIn.readInt
 
         querySelection match{
             case 1 =>   println("SELECT * FROM " + tickerQuery + " LIMIT 10")
-                        hiveCommand = "SELECT * FROM " + tickerQuery + " LIMIT 10"
+                        hiveCommand = "SELECT * FROM " + tickerQuery + " LIMIT 11"
                         var x = statement.executeQuery(hiveCommand)
                         while (x.next()){
-                            println(x.getString(1) + "  " + x.getString(3) + "  " + x.getString(4))
+                            println(x.getString(1) + "  " + x.getString(2) + "  " + x.getString(3) + "  " + x.getString(4) + "  " + x.getString(5) + "  " + x.getString(6))
                         }
-            case 2 => println("2: ") 
-            case 3 => println("3: ")            
-            case 4 => println("4: ")
+
+            case 2 =>   println("SELECT MAX(CAST(high as int)) FROM " + tickerQuery)
+                        println("Loading...")
+                        hiveCommand = "SELECT MAX(CAST(high as int)) FROM " + tickerQuery
+                        var x = statement.executeQuery(hiveCommand)
+                        while (x.next()){
+                            println("High: " + x.getString(1))
+                        }
+
+            case 3 =>   println("SELECT MIN(CAST(low as int)) FROM " + tickerQuery)    
+                        println("Loading...")
+                        hiveCommand = "SELECT MIN(CAST(low as int)) FROM " + tickerQuery
+                        var x = statement.executeQuery(hiveCommand)
+                        while (x.next()){
+                            println("Low: " + x.getString(1))
+                        }
+
+            case 4 =>   println("SELECT AVG(CAST(high as int)) FROM " + tickerQuery)    
+                        println("Loading...")
+                        hiveCommand = "SELECT AVG(CAST(high as int)) FROM " + tickerQuery
+                        var x = statement.executeQuery(hiveCommand)
+                        while (x.next()){
+                            println("AVG High: " + x.getString(1))
+                        }
+
+            case 5 =>   println("SELECT MAX(CAST(volume as int)) FROM " + tickerQuery)
+                        println("Loading...")
+                        hiveCommand = "SELECT MAX(CAST(volume as int)) FROM " + tickerQuery
+                        var x = statement.executeQuery(hiveCommand)
+                        while (x.next()){
+                            println("MAX Volume: " + x.getString(1))
+                        }
+
+            case 6 =>   println("SELECT MIN(CAST(volume as int)) FROM " + tickerQuery)
+                        println("Loading...")
+                        hiveCommand = "SELECT MIN(CAST(volume as int)) FROM " + tickerQuery
+                        var x = statement.executeQuery(hiveCommand)
+                        while (x.next()){
+                            println("MIN Volume: " + x.getString(1))
+                        }
+
+            case 7 =>   println("Type in a custom Query") 
+                        var c = scala.io.StdIn.readLine
+                        hiveCommand = s"$c"
+                        var x = statement.executeQuery(hiveCommand)
+
+                        println("How many fields would you like to display?")
+                        println("Enter a number from 2 to 7")
+
+                        var fieldNum = scala.io.StdIn.readInt
+
+                        fieldNum match{
+                            case 2 =>   while (x.next()){
+                                            println(x.getString(1) + "  " + x.getString(2))
+                                        }
+
+                            case 3 =>   while (x.next()){
+                                            println(x.getString(1) + "  " + x.getString(2) + "  " + x.getString(3))
+                                        }
+
+                            case 4 =>   while (x.next()){
+                                            println(x.getString(1) + "  " + x.getString(2) + "  " + x.getString(3) + "  " + x.getString(4))
+                                        }
+
+                            case 5 =>   while (x.next()){
+                                            println(x.getString(1) + "  " + x.getString(2) + "  " + x.getString(3) + "  " + x.getString(4) + "  " + x.getString(5))
+                                        }
+
+                            case 6 =>   while (x.next()){
+                                            println(x.getString(1) + "  " + x.getString(2) + "  " + x.getString(3) + "  " + x.getString(4) + "  " + x.getString(5) + "  " + x.getString(6))
+                                        }
+
+                            case 7 =>   while (x.next()){
+                                            println(x.getString(1) + "  " + x.getString(2) + "  " + x.getString(3) + "  " + x.getString(4) + "  " + x.getString(5) + "  " + x.getString(6) + "  " + x.getString(7))
+                                        }
+
+                            case _ =>   println("Invalid: Returning to Selections")
+                        }
+
             case _ => println("Invalid: Returning to Selections")
         }
 
